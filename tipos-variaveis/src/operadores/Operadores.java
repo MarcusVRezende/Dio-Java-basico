facd707a9976bafc4de480d46a178b564c2c0cc5
Marcus Rezende <marcusviniciusxmvr@gmail.com>
2024-09-03 21:08:26 -0300

    criando o projeto para exemplo de tipos vaariaveis

---
+++ tipos-variaveis/src/operadores/Operadores.java
@@ -0,0 +1,25 @@
+package operadores;
+public class Operadores {
+
+    public static void main(String[] args){
+
+        int a, b;
+
+        a = 6;
+        b = 6;
+
+        String resultado = "";
+/*
+        if(a == b)
+            resultado = "verdadeiro";
+        else
+            resultado = "falso";
+*/
+//      String resultado = a == b ? " verdadeiro" : " falso";
+        
+        resultado = a == b ? " verdadeiro" : " falso";
+
+        System.out.println(resultado);
+
+    }
+}
---
+++ tipos-variaveis/src/operadores/Operadores2.java
@@ -0,0 +1,16 @@
+package operadores;
+
+public class Operadores2 {
+
+    public static void main(String[] args){
+        int numero1 = 2, numero2 = 2;
+        String nome1 = "Marcus";
+        String nome2 = "Marcus";
+
+        System.out.println( nome1.equals(nome2));
+
+        boolean simNao = numero1 == numero2;
+
+        System.out.println(" Numero1 é igual a numero2? " + simNao);
+    }
+}
